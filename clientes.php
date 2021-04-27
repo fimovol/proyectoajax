24399beef684ee1096b83d9b8f4c49dadba23c0d
fimovol <fimovolpe@gmail.com>
2021-04-26 20:56:09 -0500

    agregando cleintes al proyecto

---
+++ clientes.php
@@ -0,0 +1,36 @@
+<?php
+$cn = new PDO('mysql:host=localhost;dbname=catalogo',"root","");
+
+if($_POST["Action"] == "crear"){
+    $nombre=$_POST["Nom"];
+    $edad=$_POST["Edad"];
+    $carrera=$_POST["Carrera"];
+    $statement =$cn->prepare("call crearcliente(:dato1, :dato2, :dato3)");
+    $statement->bindParam(':dato1', $_POST["Nom"]);
+    $statement->bindParam(':dato2', $_POST["Edad"]);
+    $statement->bindParam(':dato3', $_POST["Carrera"]);
+    
+    $result = $statement->execute();
+    echo "fijate en la base de datos ave si se guardo el dato";
+}
+
+
+if($_POST["Action"] == "vista"){
+    $statement = $cn->prepare("call verclientes()");
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $clientes ="";
+    if($result)
+        {
+            foreach($result as $row)
+            {
+                $clientes.= $row["nombre"].$row["edad"].$row["catalogo"];
+            }
+        }
+    else
+    {
+        $clientes= 'No existen registros en la Tabla Productos';
+    }
+        echo $clientes;
+}
+?>
